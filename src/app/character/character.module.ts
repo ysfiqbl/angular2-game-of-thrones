e1a956c57e340cf1e9cf130084424cdc868886dc
Yusuf Iqbal <ysf.iqbl@gmail.com>
2016-10-17 12:03:04 +0530

    Added FormsModule.

--- src/app/character/character.module.ts
+++ src/app/character/character.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { CharacterComponent } from './character.component';
 import { CharacterAddComponent } from './character-add/character-add.component';
@@ -12,7 +13,8 @@ import { routing } from './character.routing';
 @NgModule({
 	imports: [
 		CommonModule,
-		routing
+		routing,
+		FormsModule
 	],
 	declarations: [
 		CharacterComponent,
